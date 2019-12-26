820424ac2ccfd756738dceaa0439cbd9d14c5314
qiyancos <lsk_mprc@pku.edu.cn>
2019-12-26 14:28:27 +0800

    Change file suffix

--- build_champsim.sh
+++ build_champsim.sh
@@ -87,12 +87,12 @@ fi
 echo
 
 # Change prefetchers and replacement policy
-cp branch/${BRANCH}.bpred branch/branch_predictor.cc
-cp prefetcher/${L1I_PREFETCHER}.l1i_pref prefetcher/l1i_prefetcher.cc
-cp prefetcher/${L1D_PREFETCHER}.l1d_pref prefetcher/l1d_prefetcher.cc
-cp prefetcher/${L2C_PREFETCHER}.l2c_pref prefetcher/l2c_prefetcher.cc
-cp prefetcher/${LLC_PREFETCHER}.llc_pref prefetcher/llc_prefetcher.cc
-cp replacement/${LLC_REPLACEMENT}.llc_repl replacement/llc_replacement.cc
+cp branch/${BRANCH}.bpred.cpp branch/branch_predictor.cc
+cp prefetcher/${L1I_PREFETCHER}.l1i_pref.cpp prefetcher/l1i_prefetcher.cc
+cp prefetcher/${L1D_PREFETCHER}.l1d_pref.cpp prefetcher/l1d_prefetcher.cc
+cp prefetcher/${L2C_PREFETCHER}.l2c_pref.cpp prefetcher/l2c_prefetcher.cc
+cp prefetcher/${LLC_PREFETCHER}.llc_pref.cpp prefetcher/llc_prefetcher.cc
+cp replacement/${LLC_REPLACEMENT}.llc_repl.cpp replacement/llc_replacement.cc
 
 # Build
 mkdir -p bin
@@ -127,9 +127,9 @@ sed -i.bak 's/\<NUM_CPUS '${NUM_CORE}'\>/NUM_CPUS 1/g' inc/champsim.h
 #sed -i.bak 's/\<DRAM_CHANNELS 2\>/DRAM_CHANNELS 1/g' inc/champsim.h
 #sed -i.bak 's/\<DRAM_CHANNELS_LOG2 1\>/DRAM_CHANNELS_LOG2 0/g' inc/champsim.h
 
-cp branch/bimodal.bpred branch/branch_predictor.cc
-cp prefetcher/no.l1i_pref prefetcher/l1i_prefetcher.cc
-cp prefetcher/no.l1d_pref prefetcher/l1d_prefetcher.cc
-cp prefetcher/no.l2c_pref prefetcher/l2c_prefetcher.cc
-cp prefetcher/no.llc_pref prefetcher/llc_prefetcher.cc
-cp replacement/lru.llc_repl replacement/llc_replacement.cc
+cp branch/bimodal.bpred.cpp branch/branch_predictor.cc
+cp prefetcher/no.l1i_pref.cpp prefetcher/l1i_prefetcher.cc
+cp prefetcher/no.l1d_pref.cpp prefetcher/l1d_prefetcher.cc
+cp prefetcher/no.l2c_pref.cpp prefetcher/l2c_prefetcher.cc
+cp prefetcher/no.llc_pref.cpp prefetcher/llc_prefetcher.cc
+cp replacement/lru.llc_repl.cpp replacement/llc_replacement.cc
