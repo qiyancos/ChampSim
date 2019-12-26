53d00cf3bda4f5f3658a7b1e52c0e1cbe8642a9b
qiyancos <lsk_mprc@pku.edu.cn>
2019-12-26 13:56:41 +0800

    Modify script for trace download

--- scripts/download_dpc3_traces.sh
+++ scripts/download_dpc3_traces.sh
@@ -3,5 +3,10 @@
 mkdir -p $PWD/../dpc3_traces
 while read LINE
 do
-    wget -P $PWD/../dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+    if [ ! -f $PWD/../dpc3_traces/$LINE ]
+    then
+        wget -P $PWD/../dpc3_traces -c \
+                http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+    else echo "-- $LINE already exits. Skipped!"
+    fi
 done < dpc3_max_simpoint.txt
