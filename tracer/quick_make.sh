545ef58237eb88d31ba918b2c26d1709c0285886
qiyancos <lsk_mprc@pku.edu.cn>
2019-12-26 14:45:43 +0800

    Add pin tools to ChampSim

--- tracer/quick_make.sh
+++ tracer/quick_make.sh
@@ -1,1 +1,5 @@
-make PIN_ROOT=/home/grads/c/cienlux/cienlux/task/pin-3.0-76991-gcc-linux obj-intel64/champsim_tracer.so
+#! /bin/bash
+root=`dirname $0`
+cd $root
+root=$PWD
+make PIN_ROOT=$PWD/pin-3.2-81205-gcc-linux obj-intel64/champsim_tracer.so
